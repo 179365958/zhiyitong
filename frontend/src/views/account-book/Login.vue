7f18b2c0dea4bd3d7a18f33aef82c304e7981be9
179365958 <179365958@qq.com>
2024-12-17 10:12:14 +0800

    modified:   frontend/src/views/account-book/Login.vue

--- frontend/src/views/account-book/Login.vue
+++ frontend/src/views/account-book/Login.vue
@@ -3,13 +3,16 @@
     <el-button type="default" @click="() => $router.push('/login')" style="position: absolute; right: 20px; top: 20px;">返回</el-button>
     <el-form :model="loginForm" ref="loginFormRef" label-width="100px">
       <h2 class="login-title" style="color: #2196F3;">账套管理</h2>
-      <el-form-item label="用户名">
+      <el-form-item label="用户名" style="margin-top: 10px;">
         <el-input v-model="loginForm.username" placeholder="请输入用户名" />
       </el-form-item>
-      <el-form-item label="密码">
+      <el-form-item label="密码" style="margin-top: 10px;">
         <el-input v-model="loginForm.password" type="password" placeholder="请输入密码" />
       </el-form-item>
       <el-form-item>
+        <el-checkbox v-model="loginForm.rememberMe">记住密码</el-checkbox>
+      </el-form-item>
+      <el-form-item style="margin-top: 10px;">
         <el-button type="primary" @click="handleLogin" :loading="loading">登录</el-button>
       </el-form-item>
     </el-form>
@@ -17,20 +20,21 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { ElMessage } from 'element-plus'
 import { checkSystemInit } from '@/api/system'
 import request from '@/utils/request'
 import { setToken, setUserInfo } from '@/utils/auth'
 
-const router = useRouter()
 const loginFormRef = ref(null)
 const loading = ref(false)
+const router = useRouter()
 
 const loginForm = ref({
   username: '',
-  password: ''
+  password: '',
+  rememberMe: false
 })
 
 const handleLogin = async () => {
@@ -60,6 +64,17 @@ const handleLogin = async () => {
     if (response.success) {  
       setToken(response.data.token)
       setUserInfo(response.data)
+      
+      // 记住密码功能
+      if (loginForm.value.rememberMe) {
+        localStorage.setItem('accountBookLoginInfo', JSON.stringify({
+          username: loginForm.value.username,
+          password: btoa(loginForm.value.password) // 简单加密
+        }))
+      } else {
+        localStorage.removeItem('accountBookLoginInfo')
+      }
+      
       ElMessage.success('登录成功')
       router.push({ name: 'AccountBook' })  
     } else {
@@ -72,6 +87,17 @@ const handleLogin = async () => {
     loading.value = false
   }
 }
+
+// 页面加载时检查是否有保存的登录信息
+onMounted(() => {
+  const savedLoginInfo = localStorage.getItem('accountBookLoginInfo')
+  if (savedLoginInfo) {
+    const { username, password } = JSON.parse(savedLoginInfo)
+    loginForm.value.username = username
+    loginForm.value.password = atob(password) // 解密
+    loginForm.value.rememberMe = true
+  }
+})
 </script>
 
 <style scoped>
