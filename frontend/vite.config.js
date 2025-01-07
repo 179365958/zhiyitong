b9bee7f62f8365305fbcb7262c13fb1bb729c68f
179365958 <179365958@qq.com>
2025-01-07 15:34:22 +0800

    modified:   frontend/vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,8 @@ export default defineConfig({
     }
   },
   server: {
-    port: 5173,
+    host: '0.0.0.0',
+    port: 80,
     proxy: {
       '/api': {
         target: 'http://localhost:3000', // 后端服务地址
