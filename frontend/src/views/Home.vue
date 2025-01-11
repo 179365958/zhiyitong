940f761886795f06b39e250cc049df3f4cf5e3a2
179365958 <179365958@qq.com>
2025-01-11 11:09:27 +0800

    modified:   frontend/src/views/Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -130,7 +130,7 @@
 
 <script>
 import { getCompanyList,switchDatabase} from '@/api/system'
-import { setCurrentCompany, getCurrentCompany ,getToken} from '@/utils/auth'
+import { setCurrentCompany, getCurrentCompany } from '@/utils/auth'
 import { useUserStore } from '@/stores/user'
 
 export default {
