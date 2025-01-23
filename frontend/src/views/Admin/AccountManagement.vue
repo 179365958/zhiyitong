d57c3b1278a801df5f0f59cc180b5927a1635e66
179365958 <179365958@qq.com>
2025-01-23 17:23:13 +0800

    modified:   frontend/src/views/Admin/AccountManagement.vue

--- frontend/src/views/Admin/AccountManagement.vue
+++ frontend/src/views/Admin/AccountManagement.vue
@@ -123,10 +123,8 @@
                 value-format="yyyy-MM-dd"
               ></el-date-picker>
             </el-form-item>
+
           </el-form>
-          <template #footer>
-            <el-button type="primary" @click="nextStep">下一步</el-button>
-          </template>
         </el-tab-pane>
         <el-tab-pane label="其他信息" name="additional">
           <el-form 
@@ -151,12 +149,12 @@
               <el-input v-model="form.email" placeholder="请输入邮箱" clearable />
             </el-form-item>
           </el-form>
-          <template #footer>
-            <el-button @click="prevStep">上一步</el-button>
-            <el-button type="primary" @click="handleSubmit">创建</el-button>
-          </template>
         </el-tab-pane>
       </el-tabs>
+      <template #footer>
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="handleSubmit">创建</el-button>
+      </template>
     </el-dialog>
   </div>
 </template>
@@ -461,23 +459,11 @@ const checkCompanyCodeExists = async (companyCode) => {
   }
 };
 
-// 下一步
-const nextStep = () => {
-  activeTab.value = 'additional';
-}
-
-// 上一步
-const prevStep = () => {
-  activeTab.value = 'basic';
-}
-
 onMounted(async () => {
   await userStore.safeGetUserInfo()
   fetchCompanyList()
 })
 </script>
-
-
 
 <style scoped>
 .account-management-container {
